fc141a114e0d80b62c832f76fcfdee7eeb993016
Maria Michelle <mrmichelle.dev@gmail.com>
2025-07-10 03:00:59 -0300

    primeiro commit

---
+++ homepage/js/script.js
@@ -0,0 +1,36 @@
+const slider = document.querySelectorAll('.container');
+const btnPrev = document.getElementById('prev');
+const btnNext = document.getElementById('next');
+
+let currentSlide = 0;
+
+function hideSlider() {
+  slider.forEach(item => item.classList.remove('on'))
+}
+
+function showSlider() {
+  slider[currentSlide].classList.add('on')
+}
+
+function nextSlider() {
+  hideSlider()
+  if(currentSlide === slider.length -1) {
+    currentSlide = 0
+  } else {
+    currentSlide++
+  }
+  showSlider()
+}
+
+function prevSlider() {
+  hideSlider()
+  if(currentSlide === 0) {
+    currentSlide = slider.length -1
+  } else {
+    currentSlide--
+  }
+  showSlider()
+}
+
+btnNext.addEventListener('click', nextSlider)
+btnPrev.addEventListener('click', prevSlider)
